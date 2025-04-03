2b7f0d28407861c5c9b806a7fbe67a3b0ab7433d
debashish <debashishdash03412@gmail.com>
2025-04-03 10:18:00 +0530

    Remove App, VideoFeed, DroneControl, DroneStateDisplay components and update babel.config.js for module resolution

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
